21b88cc9c70d52fbb54917dfc57698e40a271d99
rizkynr90 <bandungcitepus22@gmail.com>
2021-10-04 01:58:39 -0700

    Pertemuan-3

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\datasiswaController;
+use App\Http\Controllers\UserController;
 
 Route::get('/s', function () {
     return 'Hallo Pak';
 });
-Route::get('/rumus-segitiga/{alas}/{tinggi}', function ($alas, $tinggi) {
+Route::get('/rumus-segitiga/{alas}/{tinggi}', function ($alas = 6 , $tinggi = 7) {
     return 'Alasnya :'.$alas. "<br>"
             .'Tingginya : ' . $tinggi;
 });
@@ -31,3 +33,9 @@ Route::group(['prefix'=>'Jurusan'], function () {
         return $nama;
     });
 });
+
+Route::get('data-siswa' ,[datasiswaController::class, 'datasiswa']);
+Route::get('data-siswi' ,[datasiswaController::class, 'datasiswi']);
+Route::get('nama' ,[datasiswaController::class, 'nama']);
+
+Route::resource('user', UserController::class);
