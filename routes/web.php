f7f734ce714ec2d075c9a0aad9e830f49ed0ceb0
rizkynr90 <bandungcitepus22@gmail.com>
2021-10-04 00:49:07 -0700

    laravel

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/s', function () {
     return 'Hallo Pak';
 });
 Route::get('/rumus-segitiga/{alas}/{tinggi}', function ($alas, $tinggi) {
-    $luas = $alas * $tinggi;
     return 'Alasnya :'.$alas. "<br>"
-    .'Tingginya : ' . $tinggi;
+            .'Tingginya : ' . $tinggi;
 });
 Route::get('/codeigniter', function () {
     return 'Hallo codeigniter';
@@ -19,4 +18,16 @@ Route::get('/nama-saya-adalah-{nama?}', function ($nama = 'Wajib diisi') {
 Route::get('/nama-framework/{nama}/{tahun}', function ($nama, $tahun) {
     return 'Nama Framework yang digunakan adalah : '.$nama ."<br>". 
     "Tahun Rilis : ".$tahun;
+});
+
+Route::group(['prefix'=>'Jurusan'], function () {
+    Route::get('/', function() {
+        return 'Berhasil';
+    });
+    Route::get('/kelas/{namakelas}', function ($namakelas) {
+        return $namakelas;
+    });
+    Route::get('/wali-kelas/{nama}', function ($nama) {
+        return $nama;
+    });
 });
